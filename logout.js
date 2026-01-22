090f5f2af7f9c91131818443ffd94c23e8f1d6d2
maisacai-csf <maisacaicsf@gmail.com>
2026-01-21 22:05:34 -0300

    Update logout.js

--- logout.js
+++ logout.js
@@ -3,5 +3,5 @@ import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 
 document.getElementById("logout").addEventListener("click", async () => {
   await signOut(auth);
-  window.location.href = "index.html";
+  window.location.href = "login.html";
 });
