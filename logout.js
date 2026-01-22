8692e65c4d08edbb9eb62b4dbea3283d7853a5c2
maisacai-csf <maisacaicsf@gmail.com>
2026-01-21 23:17:47 -0300

    Add files via upload

--- logout.js
+++ logout.js
@@ -1,7 +1,14 @@
 import { auth } from "./firebase.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-document.getElementById("logout").addEventListener("click", async () => {
-  await signOut(auth);
-  window.location.href = "login.html";
+const btnLogout = document.querySelector(".logout");
+
+btnLogout.addEventListener("click", async () => {
+  try {
+    await signOut(auth);
+    window.location.replace("login.html");
+  } catch (err) {
+    alert("Erro ao sair");
+    console.error(err);
+  }
 });
